6f60ee15a7433393b79b4f61d927f7bf55a1d271
jso8910 <mail4jasonr@gmail.com>
2025-05-04 01:51:23 +0800

    Quartz sync: May 4, 2025, 1:51 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -42,6 +42,7 @@ export const defaultContentPageLayout: PageLayout = {
       title: "Articles",
       sortFn: (a, b) => {
         if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
+          console.log(b.displayName.match(/— Part (\d*)\)$/))
           if (/— Part \d*\)$/.test(a.displayName) && /a— Part \d\)$/.test(b.displayName)) {
             return Number(b.displayName.match(/— Part (\d*)\)$/)?.[1]) - Number(a.displayName.match(/— Part (\d*)\)$/)?.[1])
           } else if (/— Part \d*\)$/.test(a.displayName)) {
